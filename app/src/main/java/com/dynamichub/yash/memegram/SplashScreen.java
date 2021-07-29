7e3af27b1611befcb4dcaaa07cadafc890d91d82
yash30401 <yashveersingh30401@gmail.com>
2021-07-29 21:08:55 +0530

    Splash Screen Full Completed

--- app/src/main/java/com/dynamichub/yash/memegram/SplashScreen.java
+++ app/src/main/java/com/dynamichub/yash/memegram/SplashScreen.java
@@ -2,15 +2,39 @@ package com.dynamichub.yash.memegram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    TextView memeGramTitle,createdBy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        memeGramTitle=findViewById(R.id.memeGramTitle);
+        createdBy=findViewById(R.id.createdBy);
+
+        Animation aniFade= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        memeGramTitle.startAnimation(aniFade);
+        createdBy.startAnimation(aniFade);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
 
     }
 }
