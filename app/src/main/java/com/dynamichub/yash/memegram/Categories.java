ead10352830f0aa0423ca7acce1ee6e56bab76ba
yash30401 <yashveersingh30401@gmail.com>
2021-08-01 13:03:16 +0530

    some memes categories implemented

---
+++ app/src/main/java/com/dynamichub/yash/memegram/Categories.java
@@ -0,0 +1,16 @@
+package com.dynamichub.yash.memegram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+
+
+}
