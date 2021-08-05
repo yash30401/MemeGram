6d8cd7b658508cd219106a5abfe74e0b21aa6e6e
yash30401 <yashveersingh30401@gmail.com>
2021-08-05 21:58:17 +0530

    about added

--- app/src/main/java/com/dynamichub/yash/memegram/Categories.java
+++ app/src/main/java/com/dynamichub/yash/memegram/Categories.java
@@ -1,17 +1,64 @@
 package com.dynamichub.yash.memegram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class Categories extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+
+    }
+
+    //toolbar menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if (id==R.id.about){
+
+
+
+            dialog=new Dialog(Categories.this);
+            dialog.setContentView(R.layout.dialogabout);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.dialogbackground));
+            }
+
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.setCancelable(true);
+            dialog.getWindow().getAttributes().windowAnimations=R.style.animation;
+            dialog.show();
+
+        }
+        return true;
     }
 
     public void MainMemes(View view){
